10dc897b6590d0ecf18ed83c44720ff8d7ee2e82
Ayush Kumar Singh <aaks.feb14@gmail.com>
2025-03-24 18:29:51 +0530

    Theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,26 @@
 import Image from "next/image";
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import ModeToggle from "@/components/ModeToggle";
 
 export default function Home() {
   return (
     <div>
       <SignedOut>
-        <SignInButton mode="modal" />
-        <SignUpButton mode="modal"/>
+        <Button>
+          <SignInButton mode="modal" />
+        </Button>
       </SignedOut>
       <SignedIn>
         <UserButton />
       </SignedIn>
+      <ModeToggle/>
     </div>
   );
 }

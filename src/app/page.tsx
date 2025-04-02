6d297ae7c6380038f0bf40ecf00e0c53c05ca72f
Ayush Kumar Singh <aaks.feb14@gmail.com>
2025-04-02 18:09:40 +0530

    delete post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
+import { getPost } from "@/actions/post.action";
+import { getDbUserId } from "@/actions/user.action";
 import CreatePost from "@/components/CreatePost";
+import PostCard from "@/components/PostCard";
 import RecommendUsers from "@/components/RecommendUsers";
 import { currentUser } from "@clerk/nextjs/server";
 
@@ -6,14 +9,23 @@ import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
   const user = await currentUser();
+  const posts = await getPost();
+  const dbUserId = await getDbUserId();
   return (
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
-      <div className="lg:col-span-6">
-        {user ? <CreatePost/> : null}
+    <div className="lg:col-span-6">
+      {user ? <CreatePost /> : null}
+
+      <div className="space-y-6">
+        {posts.map((post) => (
+          <PostCard key={post.id} post={post} dbUserId={dbUserId} />
+        ))}
       </div>
-      <div className="hidden lg:block lg:col-span-4 sticky top-20">
-        <RecommendUsers/>        
-      </div>      
     </div>
+
+    <div className="hidden lg:block lg:col-span-4 sticky top-20">
+      <RecommendUsers />
+    </div>
+  </div>
   );
 }
